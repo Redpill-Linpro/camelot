133d18dfe58f9299f52a4db99c5c22116f9365f7
Billy J. Beltran <billy@redpill-linpro.com>
2022-03-25 15:27:16 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to redpill-linpro/camelot
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/redpill_linpro/camelot_test.clj
@@ -0,0 +1,7 @@
+(ns redpill-linpro.camelot-test
+  (:require [clojure.test :refer :all]
+            [redpill-linpro.camelot :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
